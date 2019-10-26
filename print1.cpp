b88dc443f41767b5424df0b8adf8077dc6ebeeb1
Thiago Soares <thiago.lima.soares258@gmail.com>
2019-10-26 17:01:50 -0300

    merged br3 into br2; added green color to [OK]

---
+++ print1.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+
+void print1() {  
+    std::cout << "Print 1: [OK]" << std::endl;  
+}
---
+++ print2.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+
+void print2() {    
+    std::cout << "Print 2: [OK]" << std::endl;  
+}
